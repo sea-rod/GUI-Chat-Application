e7739e861cba5c3454ec7966bbcf8b5498eed635
Seamus <Sea11rod2@gmail.com>
2022-09-03 10:09:53 +0530

    added qss.py

---
+++ GUI/qss.py
@@ -0,0 +1,28 @@
+style = '''
+QMainWindow{
+    background: qlineargradient(x1:0, y1:0, x2:2, y2:1,stop:0.2 rgb(21, 30, 63), stop: 0.4 rgb(0, 0, 17), stop:1 rgb(99, 112, 116));
+    color:rgb(225, 228, 255);
+    border-radius:10;
+    border:1px solid;
+    padding:5px;
+    min-height:20px;
+    }
+QScrollArea{
+    background:transparent;
+    border-radius:10;
+    border:1 solid black;
+}    
+
+QGroupBox{
+    background:transparent;
+    border:0 
+}
+QPushButton, QLineEdit{
+    border-radius:10;
+    padding:0 5;
+}
+QLabel{
+    max-height:30px;
+    border-radius:10px;
+}
+'''
