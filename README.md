01b7bbac623ca77e00f6048f95e84942e14b3130
Seamus <Sea11rod2@gmail.com>
2022-11-01 19:26:40 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,11 +1,57 @@
-# Intro
+# 👋Introduction
+We all humans are social beings and we all feel the need to interact with each other.Here is a small GUI chat application that helps you chat with your friend
 
-This is a small Gui chat application
+# 🏃‍♂️How to run the chat apllication
+1. Clone the repository
 
-# To run
-First run the pip install -r requirements.txt command in the GUI Chat Application directory
-then run the `python main.py` file
+``` bash
+git clone https://github.com/sea-rod/GUI-Chat-Application.git
+```
+2. Change the working directory
+```bash
+cd GUI-Chat-Application
+```
+3. Install dependencies
+```bash
+pip install -r requirements.txt
+```
+or
+```bash
+python -m pip insatll -r requirements.txt 
+```
+4. Start server on the server computer
+```bash
+python ./server_socket/server.py
+``` 
+5. Enter the host as 0.0.0.0 and port number(A port number can range from  0 to 65536, but ports numbers 0 to 1024 are reserved for privileged services)
+```
+Enter the host name or ip address you to bind the server with:0.0.0.0
+Enter the port:8080
+```
+6. Start run the `main.py` from the client computer
+```bash
+python main.py
+```
+7. Enter the port and the host's ip address and hit the `Connect` button
+<img src="./Screenshots/Screenshot_1.png"/>
+## To find the host ip
+### If the sever is hosted on a linux machine then open the terminal and enter the command:
+```bash
+ifconfig
+```
+### If the server is hosted on a windows machine then open the cmd and enter the command:
+```bash
+ipconfig
+```
 
+8. Enter your name and hit the `ok` button
+<img src="./Screenshots/Screenshot_2.png"/>
 
-> NOTE:
-Still developing
+9. Thats all your good to go enjoy the app 💖!!
+<img src="./Screenshots/Screenshot_3.png"/>
+
+# 🔨Built with
+- [Pyside2](https://www.qt.io/): PySide2 is the official Python module from the Qt for Python project, which provides access to the complete Qt 5.12+ framework.
+
+# License
+This project is licensed under the MIT license - see [`License`](LICENSE.txt) file for details.
---
+++ server_socket/server.py
@@ -0,0 +1,109 @@
+import socket 
+import threading
+
+class Server:
+    '''
+    This is the server side code
+    '''
+
+    __client_soc = {}
+
+    def __init__(self, host:str = "127.0.0.1", port:int = 8080, mess_size:int = 1024*128, head:int = 20) -> None:
+        self.__host = host
+        self.__port = port 
+        self.__mess_size = mess_size
+        self.__head = head
+    
+    def listen(self):
+        '''
+        It starts listening for clients
+        '''
+        self.__server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.__server.bind((self.__host,self.__port))
+        print(f"sever listening on {self.__host}:{self.__port}")
+        print("press Clrl+c to exit..")
+        self.__server.listen(5)
+    
+    def accept(self):
+        '''
+        It accepts connection from the clients
+        '''
+        while True:
+            conn ,addr = self.__server.accept()
+            print(f"{addr} connected....")
+            conn.send(("                  15Enter your name").encode())
+            try:
+                name = conn.recv(self.__mess_size).decode()
+                self.__client_soc[conn] = name[self.__head:]
+                t1=threading.Thread(target = self.receive,args=(conn,))
+                t1.start()
+            except Exception as e:
+                print(e)
+
+
+    def receive(self, conn:socket):
+        '''
+        This function receives messaages from the clients
+        Parameters:
+            conn (socket): It holds the clients socket
+        '''
+        try:
+            while True:
+                mess = ''
+                raw_mess = conn.recv(self.__mess_size).decode()
+                mess_len = int(raw_mess[:self.__head])
+                mess += raw_mess[self.__head:]
+
+                while len(mess) < mess_len:
+                    mess += conn.recv(self.__mess_size).decode()
+
+                cl_mess = f'{self.__client_soc[conn]}:{mess}'
+                mess_len = (f'{len(cl_mess):{self.__head}}')
+                mess_len += cl_mess
+
+                if mess == "xxxclosedxxx":
+                    del self.__client_soc[conn]
+                    break
+
+                self.__send(mess_len,conn)
+
+        except Exception as e:  
+            del self.__client_soc[conn]
+            print(e)
+            
+
+
+    
+
+    def __send(self,mess:str,conn:socket):
+        '''
+        It send the message to all the clients connected
+        '''
+        for soc in self.__client_soc:
+            if soc == conn:
+                continue
+            soc.send(mess.encode())
+        print(mess)
+
+if __name__ == "__main__":
+    try:
+        ip = input("Enter the host name or ip address you to bind the server with:")
+        port = int(input("Enter the port:"))
+        ip = socket.gethostbyname(ip)
+        srv = Server(ip,port)
+        srv.listen()
+        srv.accept()
+
+
+    except socket.gaierror:
+        print("Please a enter a valid ip or host name and try again")
+
+        
+    except ValueError:
+        print("Port number should be a integer")
+    except KeyboardInterrupt:
+        print("Server closed")
+
+
+
+
